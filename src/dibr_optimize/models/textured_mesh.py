c00ec566f1ded881a822d181b83d5c1eaadf88bd
ian287913 <45512766+ian287913@users.noreply.github.com>
2023-02-27 01:15:24 +0800

    set init bg color to black

--- src/dibr_optimize/models/textured_mesh.py
+++ src/dibr_optimize/models/textured_mesh.py
@@ -58,7 +58,7 @@ class TexturedMeshModel(nn.Module):
 
         return env_sphere, mesh
 
-    def init_paint(self, init_rgb_color=(1.0, 0.0, 0.0), init_background_rgb_color=(0.0, 0.0, 1.0)):
+    def init_paint(self, init_rgb_color=(1.0, 0.0, 0.0), init_background_rgb_color=(0.0, 0.0, 0.0)):
         
         init_rgb_color = torch.tensor(list(init_rgb_color)).float().to(self.device)
         init_background_rgb_color = torch.tensor(list(init_background_rgb_color)).float().to(self.device)
